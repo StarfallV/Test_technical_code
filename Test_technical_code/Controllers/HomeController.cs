462a17087dfe92f9bfcee6e31d00436cc35a4583
StarfallV <63696126+StarfallV@users.noreply.github.com>
2023-02-23 11:24:40 +0700

    Add project files.

---
+++ Test_technical_code/Controllers/HomeController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Test_technical_code.Models;
+
+namespace Test_technical_code.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult GenerateSegitiga(string input)
+        {
+            var resp = new Response();
+
+            if (ValidateInput(input)) {
+                resp.Success = true;
+
+                var output = new List<string>();
+
+                for (int i = 0; i < input.Length; i++) {
+                    string added = "";
+                    for (int j = 0; j <= i; j++) {
+                        added += "0";
+                    }
+                    output.Add(input[i] + added);
+                }
+
+                resp.Data = output;
+            } else
+            {
+                resp.Success = false;
+            }
+            
+
+            return Json(resp, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult GenerateGanjil(string input)
+        {
+            var resp = new Response();
+
+            if (ValidateInput(input))
+            {
+                resp.Success = true;
+
+                var output = new List<string>();
+
+                for (int i = 1; i < (int.Parse(input) + 1); i++) { 
+                    if (i % 2 != 0)
+                    {
+                        output.Add(i.ToString());
+                    }
+                }
+
+                resp.Data = output;
+            }
+            else
+            {
+                resp.Success = false;
+            }
+
+
+            return Json(resp, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult GeneratePrima(string input)
+        {
+            var resp = new Response();
+
+            if (ValidateInput(input))
+            {
+                resp.Success = true;
+
+                var output = new List<string>();
+
+                for (int i = 1; i < (int.Parse(input) + 1); i++)
+                {
+                    bool prime = true;
+                    int j;
+                    for (j = 2; j <= i - 1; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            prime = false;
+                            break;
+                        }
+                    }
+                    if (prime)
+                    {
+                        output.Add(i.ToString());
+                    }
+                }
+
+                resp.Data = output;
+            }
+            else
+            {
+                resp.Success = false;
+            }
+
+
+            return Json(resp, JsonRequestBehavior.AllowGet);
+        }
+
+
+        private bool ValidateInput(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return false;
+            }
+
+            if (!int.TryParse(input, out int result))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+    }
+}
---
+++ Test_technical_code/Models/Response.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Test_technical_code.Models
+{
+    public class Response
+    {
+        public bool Success { get; set; }
+        public List<string> Data { get; set; }
+    }
+}
